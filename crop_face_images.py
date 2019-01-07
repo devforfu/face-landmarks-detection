a31534e78b1d0f45db70a18658342a67e9f58b3d
devforfu <developer.z@outlook.com>
2019-01-07 18:53:14 +0500

    fastai

--- crop_face_images.py
+++ crop_face_images.py
@@ -3,13 +3,17 @@ Takes the original UMD Faces dataset and converts into cropped images and
 landmarks stored as .txt files.
 """
 import argparse
+from multiprocessing import Pool
 import os
+from functools import partial
 import random
 
+import numpy as np
 import pandas as pd
 import PIL.Image
+from tqdm import tqdm
 
-from basedir import META
+from basedir import META, CROPPED
 
 
 SEED = 1
@@ -20,37 +24,29 @@ def main():
     args = parse_args()
     root = args.meta.parent
     faces_df = create_faces_dataframe(args.meta)
+    n = len(faces_df)
     faces_df['file'] = faces_df.file.map(lambda p: root/p)
+    faces_df['output_image'] = [args.output_dir/f'{i}.jpeg' for i in range(n)]
+    faces_df['output_points'] = [args.output_dir/f'{i}.txt' for i in range(n)]
+    run(faces_df, args)
 
 
+def run(df, args):
+    n = len(df)
+    with Pool(args.jobs) as pool:
+        records = df.to_dict(orient='records')
+        results = list(tqdm(
+            pool.imap(
+                partial(worker, pad=args.pad, convert=to_centered),
+                records),
+            total=n))
+    data = pd.DataFrame(results)
+    data.to_csv(args.output_dir/'meta.csv', index=None)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-m', '--meta',
-        default=META,
-        help='Path to the file with information about dataset files'
-    )
-    parser.add_argument(
-        '-j', '--jobs',
-        default=None,
-        help='Number of parallel workers; single-threaded mode if None'
-    )
-    parser.add_argument(
-        '-p', '--pad',
-        default=0.15,
-        help='Margin around face (as a percentage of face size)'
-    )
-    parser.add_argument(
-        '-d', '--debug',
-        default=False, action='store_true',
-        help='Run script in debugging mode'
-    )
-    args = parser.parse_args()
-    args.parallel = args.jobs is not None and args.jobs > 1
-    os.environ['PYTHONBREAKPOINT'] = 'pudb.set_trace' if args.debug else '0'
-    return args
+def worker(record, pad, convert):
+    info = create_dataset_item(record)
+    crop_and_safe(info, pad=pad, convert=convert)
 
 
 def create_faces_dataframe(filename):
@@ -67,11 +63,13 @@ def create_faces_dataframe(filename):
 
 def create_dataset_item(info):
     return {
-        'subject_id': info.subject_id,
-        'image_path': info.file,
+        'subject_id': info['subject_id'],
+        'image_path': str(info['file']),
+        'output_image': str(info['output_image']),
+        'output_points': str(info['output_points']),
         'x_pos': [info[k] for k in info.keys() if k[0] == 'p' and k[-1] == 'x'],
         'y_pos': [info[k] for k in info.keys() if k[0] == 'p' and k[-1] == 'y'],
-        'face': (info.face_x, info.face_y, info.face_width, info.face_height)}
+        'face': (info['face_x'], info['face_y'], info['face_width'], info['face_height'])}
 
 
 def to_centered(xs, ys, w, h):
@@ -79,8 +77,64 @@ def to_centered(xs, ys, w, h):
 
 
 def crop_and_safe(info, pad=None, convert=None):
-    x, y, w, h = info['face']
-    img = PIL.Image.open(info['input_path'])
+    x, y, w, h = [int(round(x)) for x in info['face']]
+    img = PIL.Image.open(info['image_path'])
+    img_h, img_w = img.size
+    if pad is not None and pad > 0:
+        dx, dy = int(w*pad), int(h*pad)
+    else:
+        dx = dy = 0
+    x -= dx
+    y -= dy
+    w += dx*2
+    h += dy*2
+    # box = (
+    #     max(0,     x),
+    #     max(0,     y),
+    #     min(img_w, x + w),
+    #     min(img_h, y + h))
+    box = x, y, x + w, y + h
+    cropped = img.crop(box=box)
+    x_pos, y_pos = [np.array(info[k]) for k in ('x_pos', 'y_pos')]
+    if convert is not None:
+        x_pos, y_pos = convert(x_pos - x, y_pos - y, w, h)
+    cropped.save(info['output_image'], format='jpeg')
+    np.savetxt(info['output_points'], np.c_[y_pos, x_pos], fmt='%.4f', delimiter=',')
+    return {'image': info['output_image'], 'points': info['output_points']}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-m', '--meta',
+        default=META,
+        help='Path to the file with information about dataset files'
+    )
+    parser.add_argument(
+        '-o', '--output-dir',
+        default=CROPPED,
+        help='Path to the output folder with cropped faces'
+    )
+    parser.add_argument(
+        '-j', '--jobs',
+        default=1, type=int,
+        help='Number of parallel workers; single-threaded mode if None'
+    )
+    parser.add_argument(
+        '-p', '--pad',
+        default=0.15, type=float,
+        help='Margin around face (as a percentage of face size)'
+    )
+    parser.add_argument(
+        '-d', '--debug',
+        default=False, action='store_true',
+        help='Run script in debugging mode'
+    )
+    args = parser.parse_args()
+    args.parallel = args.jobs is not None and args.jobs > 1
+    args.output_dir.mkdir(parents=True, exist_ok=True)
+    os.environ['PYTHONBREAKPOINT'] = 'pudb.set_trace' if args.debug else '0'
+    return args
 
 
 if __name__ == '__main__':
---
+++ plot.py
@@ -0,0 +1,49 @@
+import os
+
+from imageio import imread
+import matplotlib.pyplot as plt
+import numpy as np
+
+from basedir import NUM_LANDMARKS
+
+
+def split(target):
+    """Splits landmarks into two arrays of X and Y values."""
+    return target[:NUM_LANDMARKS//2], target[NUM_LANDMARKS//2:]
+
+
+def to_centered(xs, ys, w, h):
+    """Converts absolute landmarks coordinates into relative ones in range [-1, 1]."""
+    return 2*xs/w - 1, 2*ys/h - 1
+
+
+def to_absolute(xs, ys, w, h):
+    """Inverse function converting to_centered result back into absolute coordinates."""
+    return w*(xs + 1)/2., h*(ys + 1)/2.
+
+
+def show(root, i, ax=None):
+    """Shows i-th face and landmarks from the `root` folder."""
+
+    img = imread(root / f'{i}.jpeg')
+    pts = np.loadtxt(root / f'{i}.txt', delimiter=',')
+    h, w = img.shape[:2]
+    y, x = pts[:, 0], pts[:, 1]
+    x, y = to_absolute(x, y, w, h)
+    if ax is None:
+        f, ax = plt.subplots(1, 1, figsize=(8, 8))
+    ax.imshow(img)
+    ax.set_title(f'index={i}\n({w}w x {h}h)')
+    ax.set_axis_off()
+    ax.scatter(x, y, edgecolor='white', color='lightgreen', alpha=0.8)
+
+
+def show_random_grid(root, n=4, figsize=(10, 10)):
+    """Takes n*n random images from the dataset and shows them using grid layout."""
+
+    f, axes = plt.subplots(n, n, figsize=figsize)
+    n_images = len(os.listdir(root))//2
+    indexes = np.random.choice(n_images, size=n*n, replace=False)
+    for idx, ax in zip(indexes, axes.flat):
+        show(root, idx, ax=ax)
+    f.tight_layout(h_pad=0.05, w_pad=0.05)

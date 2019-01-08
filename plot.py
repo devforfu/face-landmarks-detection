ac5a256273abb7a8e2dd419a016b452b8ba67446
devforfu <developer.z@outlook.com>
2019-01-08 18:15:02 +0500

    Plot loss curve

--- plot.py
+++ plot.py
@@ -47,3 +47,17 @@ def show_random_grid(root, n=4, figsize=(10, 10)):
     for idx, ax in zip(indexes, axes.flat):
         show(root, idx, ax=ax)
     f.tight_layout(h_pad=0.05, w_pad=0.05)
+
+
+def plot_loss_curve(learning_rates, log_loss, zoom=None, ax=None, figsize=(10, 8)):
+    """Plots curve reflecting the dependency between learning rate and training loss."""
+
+    lrs, losses = learning_rates, log_loss
+    if ax is None:
+        f, ax = plt.subplots(1, 1, figsize=figsize)
+    if zoom is not None:
+        min_lr, max_lr = zoom
+        lrs, losses = zip(*[(x, y) for x, y in zip(lrs, losses) if min_lr <= x <= max_lr])
+    ax.plot(lrs, losses)
+    ax.set_xscale('log')
+    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter('%1.0e'))

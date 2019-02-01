9f0d3c2749625d690129c185bb97d78d95f3bce7
devforfu <developer.z@outlook.com>
2019-02-01 14:52:34 +0500

    Reading detection results from server

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -14,17 +14,28 @@ function getCameraAccess() {
                 video.srcObject = stream;
                 videoEnabled = true
             })
-            .catch((error) => {
-                let warning = document.createElement("text");
-                warning.textContent = "Cannot detect landmarks without camera access :(";
-                warning.className = "text-large warning banner";
-                let messages = document.querySelector("#messages");
-                for (let i = 0; i < messages.children.length; i++) {
-                    messages.removeChild(messages.children[i]);
-                }
-                messages.appendChild(warning);
+            .catch((err) => {
+                const message = "Cannot detect landmarks without camera access :(";
+                clearMessages();
+                writeMessage(message, "warning");
             })
         );
+    }
+}
+
+function writeMessage(message, mode="info") {
+    let node = document.createElement("li");
+    let classes = ["text-large", "banner", mode];
+    node.textContent = message;
+    node.className = classes.join(" ");
+    let messages = document.querySelector("#messages");
+    messages.appendChild(node);
+}
+
+function clearMessages() {
+    let messages = document.querySelector("#messages");
+    for (let i = 0; i < messages.children.length; i++) {
+        messages.removeChild(messages.children[i]);
     }
 }
 
@@ -70,7 +81,29 @@ function sendToServer(data) {
         body: JSON.stringify({imgBase64: data})
     });
     promise.then(r => r.json()).then(data => {
-        console.log(data);
+        if (drawingCanvas === null) { return }
+        const ok = data['success'];
+        if (!ok) {
+            writeMessage('Cannot detect any face on the picture');
+            return
+        }
+
+        const cx = drawingCanvas.getContext('2d');
+        cx.fillStyle = "green";
+        cx.strokeStyle = "blue";
+        cx.lineWidth = 3;
+
+        const result = data['result'];
+        for (let i = 0; i < result.length; i++) {
+            const [x, y, w, h] = result[i]['box'];
+            cx.strokeRect(x, y, w, h);
+            const [xs, ys] = [result[i]['x'], result[i]['y']];
+            for (let j = 0; j < xs.length; j++) {
+                cx.beginPath();
+                cx.arc(xs[j], ys[j], 2,0,2*Math.PI,true);
+                cx.fill();
+            }
+        }
     }).catch(err => {
         console.log(`Detection error! ${err}`);
     })
